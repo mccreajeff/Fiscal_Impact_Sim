2d45f7cf491ab2374c0a2b68a3fc1d4b15d6bee2
mccreajeff <mccreajeff@gmail.com>
2025-07-31 21:22:03 -0700

    Added CORS middleware
    Added CORS middleware

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,20 @@
 # fatAPI outlines startup routine and routes
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from config import settings
 
-app = FastAPI()
+app = FastAPI(title="Fiscal Impact Simulator",
+              version=settings.api_version,
+              )
 
+# ---- CORS middleware ----
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.frontend_origin],  # List[str]
+    allow_methods=["*"],                       # GET, POST, etc.
+    allow_headers=["*"],                       # Content-Type, Authorization, ...
+    allow_credentials=False,                   # Change to True if you add cookies/auth
+)
 
 @app.get("/")
 def read_root():
